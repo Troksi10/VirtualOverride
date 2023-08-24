175b11a66d90352e95fdf0db4fbbed3d3bab33a8
lLija <ilija.trokovski@gmail.com>
2023-08-25 01:21:38 +0200

    Add project files.

---
+++ VirtualOverride/Program.cs
@@ -0,0 +1,9 @@
+﻿using VirtualOverride;
+
+Dog dog = new Dog("Sarko", 12);
+Console.WriteLine($"{dog.Name} is {dog.Age} years old.");
+dog.MakeSound();
+dog.Play();
+dog.Eat();
+
+Console.ReadLine();
